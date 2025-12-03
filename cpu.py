cc82f396f51d8f87225794e449f7b39e10865f57
nicopandrex <nlippiat@uvm.edu>
2025-12-02 19:09:28 -0500

    Wrote the Cpu tick and fetch method, passing all cpu tests

--- cpu.py
+++ cpu.py
@@ -73,7 +73,10 @@ class Cpu:
             # execute...
             match self._decoded.mnem:
                 case "LOADI":
-                    pass  # complete implementation here
+                    rd = self.decoded.rd
+                    imm  = self._decoded.imm & 0xFF
+                    self._regs.execute(rd=rd,data=imm,write_enable=True)
+                    
                 case "LUI":
                     # TODO Refactor for future semester(s) if any.
                     # Cheating for compatibility with released ALU tests
@@ -86,19 +89,69 @@ class Cpu:
                     data = upper | lower
                     self._regs.execute(rd=rd, data=data, write_enable=True)
                 case "LOAD":
-                    pass  # complete implementation here
+                    rd = self._decoded.rd
+                    ra = self._decoded.ra
+                    imm = self.sext(self._decoded.imm, 8)
+                    ra_val = self._regs.execute(ra=ra)[0]
+                    addr = (ra_val + imm)
+                    data = self._d_mem.read(addr)
+                    self._regs.execute(rd=rd, data=data, write_enable=True)
+                    
+                   
                 case "STORE":
-                    pass  # complete implementation here
+                    src = self._decoded.ra
+                    rd = self._decoded.rd
+                    offset = self.sext(self._decoded.imm, 8)
+                    src_val = self._regs.execute(ra=src)[0] 
+                    ra_val = self._regs.execute(ra=rd)[0]
+                    addr = (ra_val + offset)
+                    self._d_mem.write_enable(True)
+                    self._d_mem.write(addr, src_val)
+                    self._d_mem.write_enable(False)
+
                 case "ADDI":
-                    pass  # complete implementation here
+                    self._alu.set_op("ADD")
+                    rd = self._decoded.rd
+                    ra = self._decoded.ra
+                    imm = self.sext(self._decoded.imm, 8)
+                    op_a = self._regs.execute(ra=ra)[0]
+                    result = self._alu.execute(op_a, imm)
+                    self._regs.execute(rd=rd, data=result, write_enable=True)
                 case "ADD":
-                    pass  # complete implementation here
+                    self._alu.set_op("ADD")
+                    rd = self._decoded.rd
+                    ra = self._decoded.ra
+                    rb = self._decoded.rb
+                    op_a, op_b = self._regs.execute(ra=ra, rb=rb)
+                    result = self._alu.execute(op_a, op_b)
+                    self._regs.execute(rd=rd, data=result, write_enable=True)
+                    
+                    
                 case "SUB":
-                    pass  # complete implementation here
+                    self._alu.set_op("SUB")
+                    rd = self._decoded.rd
+                    rb = self._decoded.rb
+                    ra = self._decoded.ra
+                    op_a, op_b = self._regs.execute(ra=ra,rb=rb)
+                    result = self._alu.execute(op_a, op_b)
+                    self._regs.execute(rd=rd,data=result,write_enable=True)
+                    
                 case "AND":
-                    pass  # complete implementation here
+                    self._alu.set_op("AND")
+                    rd = self._decoded.rd
+                    ra = self._decoded.ra
+                    rb = self._decoded.rb
+                    op_a, op_b = self._regs.execute(ra=ra,rb=rb)
+                    result = self._alu.execute(op_a, op_b)
+                    self._regs.execute(rd=rd,data=result,write_enable=True)
                 case "OR":
-                    pass  # complete implementation here
+                    self._alu.set_op("OR")
+                    rd = self._decoded.rd
+                    ra = self._decoded.ra
+                    rb = self._decoded.rb
+                    op_a, op_b = self._regs.execute(ra=ra,rb=rb)
+                    result = self._alu.execute(op_a, op_b)
+                    self._regs.execute(rd=rd,data=result,write_enable=True)
                 case "SHFT":
                     self._alu.set_op("SHFT")
                     rd = self._decoded.rd
@@ -112,9 +165,13 @@ class Cpu:
                         offset = self.sext(self._decoded.imm, 8)
                         self._pc += offset  # take branch
                 case "BNE":
-                    pass  # complete implementation here
+                    if not self._alu.zero:
+                        offset = self.sext(self._decoded.imm, 8)
+                        self._pc += offset
                 case "B":
-                    pass  # complete implementation here
+                    offset = self.sext(self._decoded.imm, 8)
+                    self._pc += offset
+                   
                 case "CALL":
                     self._sp -= 1  # grow stack downward
                     # PC is incremented immediately upon fetch so already
@@ -129,9 +186,11 @@ class Cpu:
                     # Get return address from memory via SP
                     # Increment SP
                     # Update PC
-                    pass  # complete implementation here
+                    ret_addr = self._d_mem.read(self._sp)
+                    self._sp += 1  
+                    self._pc = ret_addr
                 case "HALT":
-                    pass  # complete implementation here
+                    self._halt = True
                 case _:  # default
                     raise ValueError(
                         "Unknown mnemonic: " + str(self._decoded) + "\n" + str(self._ir)
@@ -147,7 +206,10 @@ class Cpu:
         self._decoded = Instruction(raw=self._ir)
 
     def _fetch(self):
-        pass  # complete implementation here
+        instr= self._i_mem.read(self._pc)
+        self._ir = instr
+        self._pc += 1
+        
 
     def load_program(self, prog):
         self._i_mem.load_program(prog)
