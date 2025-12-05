ac678733971e4fba4b9bc62db910c683f3b5faab
Cash2434 <98927203+Cash2434@users.noreply.github.com>
2025-12-04 19:19:48 -0500

    Add multiply_p2_loop.asm for multiplication logic

---
+++ asm/multiply_p2_loop.asm
@@ -0,0 +1,14 @@
+        LOADI R0, 5
+        LOADI R1, 1 
+        LOADI R2, 0
+        LOADI R3, 9
+
+LOOP:
+        SHFT  R4, R0, R1
+        STORE R4, R2, 0
+        ADDI  R1, R1, 1
+        ADDI  R2, R2, 1
+        ADDI  R3, R3, -1
+        BNE   R3, LOOP
+
+        HALT
